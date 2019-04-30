0cf59045125d7e9f5b65b10850554ee2b24c6a7e
Joane Theophile <jotheophile@gmail.com>
2019-04-30 15:55:59 -0400

    webmaker react day 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import Login from './components/user/Login';
-import Register from "./components/user/Register"
-import Profile from "./components/user/Profile"
-import WebsiteList from './components/website/WebsiteList';
 
 function App() {
   return (
-   <Router>
-      <Route exact path="/" component={Login}></Route>
-      <Route exact path="/login" component={Login}></Route>
-      <Route exact path="/register" component={Register}></Route>
-      <Route exact path="/user/:uid" component={Profile}></Route>
-      <Route exact path="/user/:uid/website" component={WebsiteList}></Route>
-   </Router>
-  );
+    <div>Hello</div>
 }
 
 export default App;
